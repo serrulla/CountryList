1e14c32439d7a931d7501643ba4c410b6e3534c5
eric <eric@serrulla.com>
2018-03-01 09:57:06 +0100

    Console logs

--- app/src/main/java/com/serrulla/eric/countrylist/ChooseCountryActivity.java
+++ app/src/main/java/com/serrulla/eric/countrylist/ChooseCountryActivity.java
@@ -1,7 +1,10 @@
 package com.serrulla.eric.countrylist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -13,15 +16,25 @@ public class ChooseCountryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_choose_country);
 
         ListView list = findViewById(R.id.list);
-        String[] countries = getResources().getStringArray(R.array.countries);
+        final String[] countries = getResources().getStringArray(R.array.countries);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
                 countries
         );
 
         list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent data = new Intent();
+                data.putExtra("country", countries[position]);
+                setResult(RESULT_OK, data);
+                finish();
+            }
+        });
 
     }
 }
--- app/src/main/java/com/serrulla/eric/countrylist/FormActivity.java
+++ app/src/main/java/com/serrulla/eric/countrylist/FormActivity.java
@@ -1,17 +1,68 @@
 package com.serrulla.eric.countrylist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class FormActivity extends AppCompatActivity {
 
+    private static final int CHOOSE_COUNTRY = 1;
+    private TextView country;
+    private TextView counter;
+
+    private int cont = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("lifecycle", "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+
+        country = findViewById(R.id.country);
+        counter = findViewById(R.id.counter);
+
+        counter.setText(Integer.toString(cont));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("lifecycle", "onStop");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("lifecycle", "onStart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("lifecycle", "onDestroy");
+
     }
 
     public void onChooseCountry(View view) {
+        Intent intent = new Intent(this, ChooseCountryActivity.class);
+        startActivityForResult(intent, CHOOSE_COUNTRY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case CHOOSE_COUNTRY:
+                if (resultCode == RESULT_OK) {
+                    country.setText(data.getStringExtra("country"));
+                }
+        }
+    }
+
+    public void onAdd(View view) {
+        cont++;
+        counter.setText(Integer.toString(cont));
     }
 }
